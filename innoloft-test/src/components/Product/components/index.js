d84f59913c006475df2923158eefed04fe4ec1fc
Kurt Guardia <kurtguardia@gmail.com>
2021-04-22 10:43:05 +0200

    PorductMainInfo set

--- innoloft-test/src/components/Product/components/index.js
+++ innoloft-test/src/components/Product/components/index.js
@@ -1,5 +1,5 @@
 export { default as ProductImage } from './ProductImage/ProductImage';
-export { default as ProductMainInfo } from './ProductMain/ProductMain';
+export { default as ProductMainInfo } from './ProductMainInfo/ProductMainInfo';
 export { default as Description } from './Description/Description';
 export { default as Attributes } from './Attribute/Attribute';
 export { default as UserInfo } from './Userinfo/UserInfo';
